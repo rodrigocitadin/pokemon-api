6497a6335b3b6a03bac53aa6ca36ca0f0a3e8a8c
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-04 20:36:14 -0300

    adjust identation

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,6 +2,6 @@ namespace PokemonApi.Models;
 
 public class Category
 {
-   public int Id { get; set; } 
-   public string Name { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
 }

cf8c07ecd511fb1dd7122308333c68ea9f95be46
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-04 20:36:52 -0300

    add Country

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -2,7 +2,8 @@ namespace PokemonApi.Models;
 
 public class Owner
 {
-   public int Id { get; set; } 
-   public string Name { get; set; }
-   public string Gym { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gym { get; set; }
+    public Country Country { get; set; }
 }

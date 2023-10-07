7843e83cfac1d4ff96d7a4270ebfa2947e0b9f00
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-07 01:17:15 -0300

    creating data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonApi.Models;
+
+namespace PokemonApi.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Pokemon> Pokemons { get; set; }
+    public DbSet<PokemonOwner> PokemonOwners { get; set; }
+    public DbSet<PokemonCategory> PokemonCategories { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Reviewer> Reviewers { get; set; }
+}

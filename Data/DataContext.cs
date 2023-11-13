5bb80d1b527281a3dea645ca1387d701b5a05f5e
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-13 17:10:15 -0300

    added Rating to Reviews, and some issues with docker and wsl2, I will work on it later

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,9 +5,8 @@ namespace PokemonApi.Data;
 
 public class DataContext : DbContext
 {
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
-    {
-    }
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options) { }
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Country> Countries { get; set; }
@@ -20,24 +19,26 @@ public class DataContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<PokemonCategory>()
-            .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
-        modelBuilder.Entity<PokemonCategory>()
+        modelBuilder.Entity<PokemonCategory>().HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+        modelBuilder
+            .Entity<PokemonCategory>()
             .HasOne(p => p.Pokemon)
             .WithMany(pc => pc.PokemonCategories)
             .HasForeignKey(p => p.PokemonId);
-        modelBuilder.Entity<PokemonCategory>()
+        modelBuilder
+            .Entity<PokemonCategory>()
             .HasOne(c => c.Category)
             .WithMany(pc => pc.PokemonsCategories)
             .HasForeignKey(c => c.CategoryId);
 
-        modelBuilder.Entity<PokemonOwner>()
-            .HasKey(po => new { po.PokemonId, po.OwnerId });
-        modelBuilder.Entity<PokemonOwner>()
+        modelBuilder.Entity<PokemonOwner>().HasKey(po => new { po.PokemonId, po.OwnerId });
+        modelBuilder
+            .Entity<PokemonOwner>()
             .HasOne(p => p.Pokemon)
             .WithMany(po => po.PokemonOwners)
             .HasForeignKey(p => p.PokemonId);
-        modelBuilder.Entity<PokemonOwner>()
+        modelBuilder
+            .Entity<PokemonOwner>()
             .HasOne(o => o.Owner)
             .WithMany(po => po.PokemonOwners)
             .HasForeignKey(o => o.OwnerId);
--- Program.cs
+++ Program.cs
@@ -11,13 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder
+    .Services
+    .AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 
 var app = builder.Build();
 

c8834ffd31a48f14a2e0b0dfc945620a756fd748
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-16 17:49:05 -0300

    get a pokemon implementation

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -5,7 +5,7 @@ namespace PokemonApi.Interfaces;
 public interface IPokemonRepository
 {
     ICollection<Pokemon> GetPokemons();
-    Pokemon GetPokemon(int id);
+    Pokemon? GetPokemon(int id);
     Pokemon GetPokemon(string name);
     decimal GetPokemonRating(int PokemonId);
     bool PokemonExists(int PokemonId);
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -18,10 +18,10 @@ public class PokemonRepository : IPokemonRepository
         .OrderBy(p => p.Id)
         .ToList();
 
-    public Pokemon GetPokemon(int id)
-    {
-        throw new NotImplementedException();
-    }
+    public Pokemon? GetPokemon(int id) => _context
+        .Pokemons
+        .Where(p => p.Id == id)
+        .FirstOrDefault();
 
     public Pokemon GetPokemon(string name)
     {

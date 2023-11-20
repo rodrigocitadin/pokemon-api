b59574f90f1c8ef90b898a1710ea30b302a5f2ef
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-20 10:59:56 -0300

    finish get methods in category repository

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -13,13 +13,9 @@ public class CategoryRepository : ICategoryRepository
         _context = context;
     }
 
-    public ICollection<Category> GetCategories()
-    {
-        throw new NotImplementedException();
-    }
+    public ICollection<Category> GetCategories() => _context.Categories.ToList();
 
-    public Category? GetCategory(int id)
-    {
-        throw new NotImplementedException();
-    }
+    public Category? GetCategory(int id) => _context.Categories.First(c => c.Id == id);
+
+    public Category? GetCategory(string name) => _context.Categories.First(c => c.Name == name);
 }

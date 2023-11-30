1796a80e235c052f046c40344a4dde5b9532c9a6
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-30 10:06:05 -0300

    create review dto

---
+++ Dto/ReviewDto.cs
@@ -0,0 +1,9 @@
+namespace PokemonApi.Dto;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public decimal Rating { get; set; }
+}

c79bc1e519d395738ca37f1ead72f9e8c29379c7
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-30 10:05:56 -0300

    create owner dto

---
+++ Dto/OwnerDto.cs
@@ -0,0 +1,11 @@
+using PokemonApi.Models;
+
+namespace PokemonApi.Dto;
+
+public class Owner
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gym { get; set; }
+    public Country Country { get; set; }
+}

bdc03ea89e3126a3c60d750ed40719dd55a719c3
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-30 09:58:37 -0300

    all gets to country route

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PokemonApi.Interfaces;
+using PokemonApi.Models;
 
 namespace PokemonApi.Controllers;
 
@@ -15,5 +16,43 @@ public class CountryController : Controller
     {
         _countryRepository = countryRepository;
         _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+    public IActionResult GetCountries()
+    {
+        var countries = _mapper.Map<List<Country>>(_countryRepository.GetCountries());
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(countries);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(Country))]
+    [ProducesResponseType(400)]
+    public IActionResult GetCountry(int id)
+    {
+        var country = _mapper.Map<Country>(_countryRepository.GetCountry(id));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(country);
+    }
+
+    [HttpGet("{name}")]
+    [ProducesResponseType(200, Type = typeof(Country))]
+    [ProducesResponseType(400)]
+    public IActionResult GetCountry(string name)
+    {
+        var country = _mapper.Map<Country>(_countryRepository.GetCountry(name));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(country);
     }
 }
